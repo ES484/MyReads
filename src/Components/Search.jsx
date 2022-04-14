b82b77d796a2717317272efcc62df415e9d634f4
ES484 <esraasaad478@gmail.com>
2022-04-14 15:57:48 +0200

    update search page

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,17 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link} from 'react-router-dom';
 import {search} from '../BooksAPI';
 import Book from './Book';
+import { MediaContext } from './MediaContext';
 export default function Search(props) {
   const [books, setBooks] = useState(null);
+  const {allBooks}= useContext(MediaContext);
   async function handleInsertData(e)
   {
     try {let query= e.target.value;
       let result = await search(query);
       //Update it to allow search more than one string
-      result = result.filter((book)=> {return book.title.toLowerCase().includes(query.toLowerCase())});
-      setBooks(result);
-    } catch (error) {
+      result = result.filter((book)=> {return book.title.toLowerCase().includes(query.toLowerCase());
+      });
+      const bookWithShelf = result.map((book)=>{
+         const bookShelf= allBooks.find((b)=> b.id === book.id);
+         return {...book, shelf: bookShelf? bookShelf.shelf: 'none'};
+      });
+      setBooks(bookWithShelf);
+    } 
+    catch (error) {
       setBooks([]);
     }
     }
